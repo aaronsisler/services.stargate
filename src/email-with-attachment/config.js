b4605a20a3239f45fa1995fcc3f785e1a6a27331
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-01-02 10:18:07 -0500

    Renaming files to index for consistency

---
+++ src/email-with-attachment/config.js
@@ -0,0 +1,3 @@
+const SERVICE_EMAIL_ADDRESS = "aaron.sisler@eandbsolutions.com";
+
+exports.SERVICE_EMAIL_ADDRESS = SERVICE_EMAIL_ADDRESS;
---
+++ src/email-with-attachment/index.js
@@ -0,0 +1,23 @@
+exports.handler = async ({ body: rawData }, context, callback) => {
+  const { validateInputs } = require("./validate-inputs");
+  const { sendEmail } = require("./email-with-attachment-service");
+  const {
+    get200Response,
+    get400Response,
+    get500Response
+  } = require("./response");
+
+  const data = JSON.parse(rawData);
+
+  if (!validateInputs(data)) {
+    callback(null, get400Response());
+  }
+
+  try {
+    await sendEmail(data);
+    callback(null, get200Response());
+  } catch (error) {
+    console.log(error);
+    callback(null, get500Response());
+  }
+};
