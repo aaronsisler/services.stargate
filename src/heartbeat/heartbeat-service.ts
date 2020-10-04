8dd6db53a6f0b464d051acf2c3e99c427f7ebff2
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-10-04 17:02:09 -0400

    Moving files around and making changes for shared pieces

--- src/heartbeat/heartbeat-service.ts
+++ src/heartbeat/heartbeat-service.ts
@@ -1,14 +1,7 @@
-const heartbeatService = () => {
+const heartbeatService = (): string => {
   const currentTime = new Date().toTimeString();
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello, it looks like the gateway is working.",
-      currentTime: `The current time is ${currentTime}.`,
-    }),
-  };
 
-  return response;
+  return `Hello, it looks like the gateway is working. The current time is ${currentTime}.`;
 };
 
 export { heartbeatService };
