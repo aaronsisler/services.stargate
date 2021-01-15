5d688fa1db0392c989b63454a23ac1cc5edf28fd
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-01-14 20:22:53 -0500

    Updating the name of heartbeat to health

--- src/health/health-service.ts
+++ src/health/health-service.ts
@@ -1,7 +1,7 @@
-const heartbeatService = (): string => {
+const healthService = (): string => {
   const currentTime = new Date().toTimeString();
 
   return `Hello, it looks like the gateway is working. The current time is ${currentTime}.`;
 };
 
-export { heartbeatService };
+export { healthService };
